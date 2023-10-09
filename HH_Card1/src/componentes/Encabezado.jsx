50851ed67891701e12c8704c8675fe85e21eeb61
KeiviVeizan <kveizanp@est.emi.edu.bo>
2023-10-09 18:48:27 -0400

    avance

---
+++ HH_Card1/src/componentes/Encabezado.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Encabezado() {
+  return (
+    <div className='Encabezado'>
+        <p>
+            Este es el primer commit
+        </p>
+    </div>
+  )
+}
+
+export default Encabezado
