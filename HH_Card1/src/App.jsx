5e78c14931da570d4a69a328d5bcb1ca702c7e1c
Vega <kikivega2002@gmail.com>
2023-10-11 20:02:31 -0400

    inicio barra de busqueda

--- HH_Card1/src/App.jsx
+++ HH_Card1/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function SearchBar({ onSearch }) {
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="search-bar-container">
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Buscar..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button className="search-button" onClick={handleSearch}>
+        <img src="./Filter.jpg" alt="imagen-filtros" />
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default SearchBar;
