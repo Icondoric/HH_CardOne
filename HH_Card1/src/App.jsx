aae8f9f2a26f4b8f0b4758023ba2ed40e6d6cbb6
KeiviVeizan <kveizanp@est.emi.edu.bo>
2023-10-11 22:24:56 -0400

    arreglosx2

---
+++ HH_Card1/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Encabezado from './componentes/Encabezado'
+function App() {
+  return (
+    <div><Encabezado></Encabezado></div>
+  )
+}
+
+export default App
--- HH_Card1/src/componentes/Encabezado.jsx
+++ HH_Card1/src/componentes/Encabezado.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import './App.css'
+import '../App.css'
 
 function Encabezado() {
   return (
     <article>
       <div className='Encabezado'>
-        <h2>
-          You are here
-        </h2>
+        <p><b>You are here</b></p>
         <p>
           Indonesia
         </p>
-        <img src="http://www.w3.org/2000/svg" alt="Icono pa bajar" />
+        <img src="./Arrow.jpg" alt="Icono pa bajar" />
       </div>
       <div className='imagen'>
-        <img src="http://www.w3.org/2000/svg" alt="Imagen" />
+        <img src="./User.jpg" alt="Imagen" />
       </div>
     </article>
   )
