23a0021e7f655d331b7065991709e88e377f7112
Jose-Rojas-Jr <jr.joss1999@gmail.com>
2023-10-11 20:23:35 -0400

    iconos comentarioh3

--- HH_Card1/src/components/textoh3.jsx
+++ HH_Card1/src/components/textoh3.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Styles/styletexttoh3.css';
 
-function TextH3(props) {
-  const { text, icon } = props;
+function TextH3(frase) {
+  const { text, icon } = frase;
 
   return (
-    <h3>
-      {icon && <FontAwesomeIcon icon={icon} />}{text}
+    <h3 className={styles['texth3']}>
+      {icon && <FontAwesomeIcon icon={icon} className={styles.icon} />}{text}
     </h3>
   );
 }
+/*<FontAwesomeIcon icon="fa-light fa-bed-front" /> para el icono de cama */
+/*<FontAwesomeIcon icon="fa-solid fa-arrows-maximize" /> para el icono de espacio tamaño */
 
 export default TextH3;
