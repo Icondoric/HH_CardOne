c3e7c67bdf478372827d37b9b1f7931550fae44c
KeiviVeizan <kveizanp@est.emi.edu.bo>
2023-10-11 20:17:45 -0400

    fases finales

--- HH_Card1/src/componentes/Encabezado.jsx
+++ HH_Card1/src/componentes/Encabezado.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 
 function Encabezado() {
   return (
-    <div className='Encabezado'>
+    <article>
+      <div className='Encabezado'>
+        <h2>
+          You are here
+        </h2>
         <p>
-            
+          Indonesia
         </p>
-    </div>
+        <i cal></i>
+      </div>
+      <div className='img'>
+        <img src="http://www.w3.org/2000/svg" alt="Imagen" />
+      </div>
+    </article>
   )
 }
 
