69b73078c81fce1b861419b0d8382f827f414f95
Andrew Segales <andysejas444@gmail.com>
2023-10-12 02:03:31 -0400

    medioAcabado

---
+++ HH_Card1/src/componentes/Button.jsx
@@ -0,0 +1,18 @@
+import './Styles/Button.css'
+import React from 'react'
+function Button(  )
+{
+    return(
+      <article className="article">
+          <div className='Button1'>
+            <img src="./Star.jpg" alt="icono-estrella" />
+            <p>4.93</p>
+          </div>
+        <div className='cora'>
+          <img src="./iconFavorite.jpg" alt="corazon" />
+        </div>
+      </article>
+    )
+}
+export default Button
+
--- HH_Card1/src/main.jsx
+++ HH_Card1/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+/*import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
