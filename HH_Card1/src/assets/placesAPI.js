638472ccc17d23aeb76eb034d54e99434cbacd97
Icondoric <iconhu.icc@gmail.com>
2023-10-11 22:36:08 -0400

    modificacion de stiles

---
+++ HH_Card1/src/assets/placesAPI.js
@@ -0,0 +1,5 @@
+export const places =[
+    {
+        title:"Hola"
+    }
+]
---
+++ HH_Card1/src/codigo/Codigo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Codigo(promps) {
+    const places=promps.places
+
+  return (
+    <div>Codigo</div>
+  )
+}
+
+export default Codigo
